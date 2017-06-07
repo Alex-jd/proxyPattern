e6c489aaa87ce7615be9ff658762cbfb44915dc2
alex_jd <twoalexan@mail.ru>
2017-06-07 23:57:26 +0500

    implement the Protect Proxy pattern

---
+++ src/service/PersonBean.java
@@ -0,0 +1,13 @@
+package service;
+
+public interface PersonBean {
+	
+	String getName();
+	String getInterests();
+	int getHotOrNotRating();
+	
+	void setName(String name);
+	void setInterests(String interests);
+	void setHotOrNotRating(int rating);
+
+}
